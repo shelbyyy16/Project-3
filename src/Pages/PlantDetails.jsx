f77988c6510268781548ceb6b6c2245f45edb531
honeybee <shelby.e.pagan@gmail.com>
2023-12-18 12:14:42 -0500

    add a PlantDetails function on details page

--- src/Pages/PlantDetails.jsx
+++ src/Pages/PlantDetails.jsx
@@ -1,8 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
 function PlantDetails() {
+    const [plantDetails, setPlantDetails] = useState(null);
+    const { id }= useParams
+
+    useEffect(() => {
+        async function fetchPlantDetails() {
+            const plantId = match.params.id;
+
+            try {
+                const apiKey = process.env.API_KEY;
+                const response = await axios.get(`https://perenual.com/api/plant-details/${plantId}?key=${apiKey}`);
+                setPlantDetails(response.data);
+            } catch (error) {
+                console.error("Error fetching plant details:", error);
+            }
+        }
+        
+        fetchPlantDetails();
+    }, [match.params.id]);
 
     return (
-        <h1>Plant Details Page</h1>
-    )
+        <div>
+            <h1>Plant Details</h1>
+            {plantDetails && (
+                <div>
+                    <h2>{plantDetails.common_name}</h2>
+                    <p>Scientific Name: {plantDetails.scientific_name[0]}</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default PlantDetails;
--- src/Pages/Plants.jsx
+++ src/Pages/Plants.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 function Plants() {
-    const [data, setData] = useState(null);
+    const [plants, setPlants] = useState(null);
 
     useEffect(() => {
         async function fetchPlants() {
             try {
-                const apiKey = rocess.env.API_KEY;
+                const apiKey = process.env.REACT_APP_API_KEY;
                 const response = await axios.get(`https://perenual.com/api/species-list?key=${apiKey}`);
-                setData(response.data);
+                setPlants(response.data);
             } catch (error) {
-                console.error("Error fetching data:", error);
+                console.error("Error fetching plant:", error);
             }
         }
 
@@ -20,20 +20,23 @@ function Plants() {
     }, []);
 
   return (
-    <section className="container">
-      <h1>API Data</h1>
-      {data && data.data.map((plant) => (
-          <Link to={`/details/${plant._id}`} key={plant.id}>
-            <div className="card">
-              <div className="plant-image">
-                <img src={plant.default_image.regular_url} />
+    <>
+      <h1>Plant Library</h1>
+      <section className="container">
+        {plants &&
+          plants.data.map((plant) => (
+            <Link to={`/details/${plant._id}`} key={plant.id}>
+              <div className="card">
+                <div className="plant-image">
+                  <img src={plant.default_image.regular_url} />
+                </div>
+                <h2>{plant.common_name}</h2>
+                <p>{plant.scientific_name[0]}</p>
               </div>
-              <h2>{plant.common_name}</h2>
-              <p>{plant.scientific_name[0]}</p>
-            </div>
-          </Link>
-        ))}
-    </section>
+            </Link>
+          ))}
+      </section>
+    </>
   );
 }
 
