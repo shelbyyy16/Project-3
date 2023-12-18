db25afefde66499564be41b178e3eed0139c9fa4
honeybee <shelby.e.pagan@gmail.com>
2023-12-18 15:06:43 -0500

    Build  Auth0ProviderNaviagtion

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Routes,  Route } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './Pages/Home';
-import Plants from './Pages/Plants';
-import PlantDetails from './Pages/PlantDetails';
-import MyGarden from './Pages/MyGarden';
+import Header from '../Header';
+import Footer from '../Footer';
+import Home from '../../Pages/Home';
+import Plants from '../../Pages/Plants';
+import PlantDetails from '../../Pages/PlantDetails';
+import MyGarden from '../../Pages/MyGarden';
 
 
 function App() {
