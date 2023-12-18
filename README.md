7b7d0ac3e61b951ca642dd832043aa4b315dc7f6
honeybee <shelby.e.pagan@gmail.com>
2023-12-18 11:51:05 -0500

    update fetchPlants function to call API

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 
+## Petals $ Pots 
+Contains a description of what the app does and optional background info.
+
+User Flow(s): A screenshot of your app’s landing page and the core features in your app.
+
+Note: If your user needs take any specific steps to navigate, interact, or activate your application’s services, include that information in this section.
+
+## Technologies Used:
+- React
+- Javascript
+- 
+
+Getting Started: That includes:
+
+A link to the deployed app (Netlify)
+
+[Trello Board]("https://trello.com/b/SdwL81cx/plant-app")
+
+Note: If you have special requirements for setting up the tool (such as providing an API key) - provide all relevant instructions in this section.
+
+Next Steps: 
+- 
+- 
+- 
--- src/Pages/Plants.jsx
+++ src/Pages/Plants.jsx
@@ -1,33 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Plants() {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        async function fetchData() {
+        async function fetchPlants() {
             try {
-                const response = await axios.get(`https://perenual.com/api/species-list?key=sk-dYCf657f152c83b433491`);
+                const apiKey = rocess.env.API_KEY;
+                const response = await axios.get(`https://perenual.com/api/species-list?key=${apiKey}`);
                 setData(response.data);
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
         }
-        
-        fetchData();
+
+        fetchPlants();
     }, []);
 
-    return (
-        <div>
-            <h1>Plant Library</h1>
-            {data && data.data.map((plant) => (
-                <div key={plant.id}>
-                    <h2>{plant.common_name}</h2>
-                    <p>Scientific Name: {plant.scientific_name[0]}</p>
-                </div>
-            ))}
-        </div>
-    );
+  return (
+    <section className="container">
+      <h1>API Data</h1>
+      {data && data.data.map((plant) => (
+          <Link to={`/details/${plant._id}`} key={plant.id}>
+            <div className="card">
+              <div className="plant-image">
+                <img src={plant.default_image.regular_url} />
+              </div>
+              <h2>{plant.common_name}</h2>
+              <p>{plant.scientific_name[0]}</p>
+            </div>
+          </Link>
+        ))}
+    </section>
+  );
 }
 
 export default Plants;
