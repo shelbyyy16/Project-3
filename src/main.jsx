ace94b79a83b42ac73b9282728c00e70c002df57
honeybee <shelby.e.pagan@gmail.com>
2023-12-18 15:09:12 -0500

    import Auth0 to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import Auth0ProviderWithNavigate from './components/Auth/Auth0-provider-with-navigate.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
-    <App />
+        <Auth0ProviderWithNavigate>
+        <App />
+        </Auth0ProviderWithNavigate>
+    
     </Router>
 )
