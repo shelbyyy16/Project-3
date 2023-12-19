f0ce5c5aa464707e969e052141451d06227b1399
honeybee <shelby.e.pagan@gmail.com>
2023-12-19 15:09:55 -0500

    css for profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate } from "react-router";
-
-
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -17,10 +15,16 @@ const Profile = () => {
 
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={""} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+      <div className="profile-container">
+        <div className="profile-card">
+          <div className="profile-image">
+            <img src={user.picture} alt={""} />
+          </div>
+          <div className="profile-details">
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        </div>
       </div>
     )
   );
