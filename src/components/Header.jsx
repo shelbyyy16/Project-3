189cbf5db52899514b016b06f7e1b996197f5e94
honeybee <shelby.e.pagan@gmail.com>
2023-12-19 14:26:56 -0500

    add home icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,35 +13,38 @@ function Header({ setSearchQuery }) {
 
   return (
     <header>
-      <div>
-        <nav className="nav">
-          <Link to="/">
-            <div className="home-nav">Petals & Pots</div>
-          </Link>
-          <div className="login-logout-button">
-            {/* A nested ternary can conditionally render multiple states */}
-            {!isLoading ? (
-              isAuthenticated ? (
-                <span>
-                  <Link to="/profile">Profile</Link> || <LogoutButton />
-                </span>
-              ) : (
-                <LoginButton />
-              )
-            ) : null}
-          </div>
-          <input
-            type="text"
-            placeholder="Search for plants..."
-            onChange={handleSearch}
-          />
-          <Link to="/search">
-            <div className="search-nav">Search</div>
-          </Link>
-        </nav>
-      </div>
+        <div>
+            <nav className="nav">
+                <Link to="/">
+                    <img
+                        src="./images/plantIcon.png" 
+                        alt="Home"
+                        className="home-icon"
+                    />
+                </Link>
+                <div className="login-logout-button">
+                    {!isLoading ? (
+                        isAuthenticated ? (
+                            <span>
+                                <Link to="/profile">Profile</Link> || <LogoutButton />
+                            </span>
+                        ) : (
+                            <LoginButton />
+                        )
+                    ) : null}
+                </div>
+                <input
+                    type="text"
+                    placeholder="Search for plants..."
+                    onChange={handleSearch}
+                />
+                <Link to="/search">
+                    <div className="search-nav">Search</div>
+                </Link>
+            </nav>
+        </div>
     </header>
-  );
-}
+)
+};
 
 export default Header;
