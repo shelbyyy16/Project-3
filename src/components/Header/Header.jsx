41566606a7b5c3557190278cb6fdf02242fe0e31
honeybee <shelby.e.pagan@gmail.com>
2023-12-21 21:09:39 -0500

    Auth0 test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Header.css"
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "../Auth/LoginButton";
